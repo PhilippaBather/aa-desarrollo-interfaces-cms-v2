8f1e8d658a38717e9d08531ba44fc2bcaf10e384
pabather <p.a.bather1@gmail.com>
2024-01-11 23:36:35 +0100

    refactored

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-use controller\RutaController;
-
 require "init.php";
 
+use controller\AutenticacionController;
+
 // establece el controlador para comprobar autenticación
-$load_controller = new RutaController();
+$load_controller = new AutenticacionController();
 $load_view = $load_controller->comprobarAutenticacion();
 // carga la vista depende del estado de autenticación
 $load_view->pintarView();
